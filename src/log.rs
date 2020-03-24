098545cc657b49231d2367062c5a01f4cc26cc9b
RanolP <public.ranolp@gmail.com>
2020-03-25 01:36:06 +0900

    feat: try replace rocket with actix

---
+++ src/log.rs
@@ -0,0 +1,20 @@
+use chrono::Local;
+use log::LevelFilter;
+
+pub fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("output.log")?)
+        .apply()?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -4,52 +4,77 @@
 #[macro_use]
 extern crate diesel;
 
-use rocket::response::content::Html;
-use rocket::State;
-use rocket::{get, post, routes};
+use actix_web::{dev, http, middleware, web, App, Error, HttpResponse, HttpServer, Responder};
 
 use crate::gql::{Context, Mutation, Query, Schema};
+use actix_rt;
+use actix_web::middleware::errhandlers::ErrorHandlers;
+use juniper::http::{playground::playground_source, GraphQLRequest};
+use middleware::errhandlers::ErrorHandlerResponse;
+use std::io;
+use std::sync::Arc;
 
 mod config;
 mod database;
 mod gql;
+mod log;
 
 /// A playground for developers
-#[get("/")]
-fn playground() -> Html<String> {
-    juniper_rocket::playground_source("/graphql")
+async fn playground() -> impl Responder {
+    let html = playground_source("/graphql");
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
 }
 
-/// GraphQL handler for GET requests
-#[get("/graphql?<request>")]
-fn get_graphql_handler(
-    context: State<Context>,
-    request: juniper_rocket::GraphQLRequest,
-    schema: State<Schema>,
-) -> juniper_rocket::GraphQLResponse {
-    request.execute(&schema, &context)
+/// GraphQL handler
+async fn graphql(
+    schema: web::Data<Arc<Schema>>,
+    context: web::Data<Arc<Context>>,
+    data: web::Json<GraphQLRequest>,
+) -> Result<HttpResponse, Error> {
+    let user = web::block(move || {
+        let res = data.execute(&schema, &context);
+        Ok::<_, serde_json::error::Error>(serde_json::to_string(&res)?)
+    })
+    .await?;
+    Ok(HttpResponse::Ok()
+        .content_type("application/json")
+        .body(user))
 }
 
-/// GraphQL handler for POST requests
-#[post("/graphql", data = "<request>")]
-fn post_graphql_handler(
-    context: State<Context>,
-    request: juniper_rocket::GraphQLRequest,
-    schema: State<Schema>,
-) -> juniper_rocket::GraphQLResponse {
-    request.execute(&schema, &context)
+fn render_404<B>(mut res: dev::ServiceResponse<B>) -> Result<ErrorHandlerResponse<B>, Error> {
+    dbg!(res.request());
+    res.response_mut().headers_mut().insert(
+        http::header::CONTENT_TYPE,
+        http::HeaderValue::from_static("Error"),
+    );
+    Ok(ErrorHandlerResponse::Response(res))
 }
 
-fn main() {
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
     let configuration = config::Config::load().expect("Invalid configuration detected");
 
-    database::establish_connection(&configuration.database).expect("Invalid database configuration detected");
-    rocket::ignite()
-        .manage(Context::new())
-        .manage(Schema::new(Query, Mutation))
-        .mount(
-            "/",
-            routes![playground, get_graphql_handler, post_graphql_handler],
-        )
-        .launch();
+    database::establish_connection(&configuration.database)
+        .expect("Invalid database configuration detected");
+
+    log::setup_logger().expect("Logger setup failed");
+
+    let context = Arc::new(Context::new());
+    let schema = Arc::new(Schema::new(Query, Mutation));
+
+    HttpServer::new(move || {
+        App::new()
+            .data(schema.clone())
+            .data(context.clone())
+            .wrap(middleware::Logger::default())
+            .wrap(ErrorHandlers::new().handler(http::StatusCode::NOT_FOUND, render_404))
+            .service(web::resource("/graphql").route(web::post().to(graphql)))
+            .service(web::resource("/graphql").route(web::get().to(graphql)))
+            .service(web::resource("/").route(web::get().to(playground)))
+    })
+    .bind("127.0.0.1:80")?
+    .run()
+    .await
 }
