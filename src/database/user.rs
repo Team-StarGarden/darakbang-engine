15196870dd023cb42b4bc739ef24761c36c1a371
kiwiyou <kiwiyou.dev@gmail.com>
2020-03-26 23:16:34 +0900

    add: Add function find_local_user

--- src/database/user.rs
+++ src/database/user.rs
@@ -2,7 +2,7 @@ use crate::database::model::{NewUser, User};
 use crate::diesel::RunQueryDsl;
 use diesel::mysql::MysqlConnection;
 use diesel::result::QueryResult;
-use diesel::QueryDsl;
+use diesel::{ExpressionMethods, OptionalExtension, QueryDsl};
 
 pub fn create_user(
     conn: &MysqlConnection,
@@ -21,5 +21,35 @@ pub fn create_user(
         diesel::insert_into(user).values(&new_user).execute(conn)?;
 
         user.find(uid).get_result(conn)
+    }
+}
+
+pub fn find_local_user(
+    conn: &MysqlConnection,
+    user_name: &str,
+    password: &str,
+) -> QueryResult<Option<User>> {
+    use crate::database::schema::user::dsl::{self, user};
+    use zeroize::Zeroizing;
+
+    let salt: Zeroizing<_> = user
+        .select(dsl::salt)
+        .filter(dsl::user_name.eq(user_name))
+        .first::<Option<String>>(conn)
+        .optional()?
+        .flatten()
+        .into();
+        
+    if let Some(salt) = salt.as_ref() {
+        let password_hash = Zeroizing::new(argon2rs::argon2i_simple(password, salt));
+        let result = user
+            .filter(dsl::user_name.eq(user_name))
+            .filter(dsl::salt.eq(salt))
+            .filter(dsl::password.eq(Some(password_hash.as_ref())))
+            .first(conn)
+            .optional()?;
+        Ok(result)
+    } else {
+        Ok(None)
     }
 }
