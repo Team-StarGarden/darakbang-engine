cbb3d7bc99c72bd0622b8bf1e664e47cbe515224
RanolP <public.ranolp@gmail.com>
2020-03-26 15:40:48 +0900

    feat(log): Make log prettier

--- src/log.rs
+++ src/log.rs
@@ -1,15 +1,35 @@
 use chrono::Local;
-use log::LevelFilter;
+use colored::*;
+use log::{Level, LevelFilter};
 
 pub fn setup_logger() -> Result<(), fern::InitError> {
     fern::Dispatch::new()
         .format(|out, message, record| {
             out.finish(format_args!(
-                "{}[{}][{}] {}",
-                Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
-                record.target(),
-                record.level(),
-                message
+                "{time} {target} {arrow} {level} {message}",
+                time = Local::now()
+                    .format("[%Y-%m-%d] [%H:%M:%S]")
+                    .to_string()
+                    .bright_black(),
+                target = format!("[{}]", record.target().bright_black()),
+                arrow = "›".bright_black(),
+                level = {
+                    let (color, icon, label) = match record.level() {
+                        Level::Info => (Color::Blue, "i", "info"),
+                        Level::Warn => (Color::Yellow, "⚠", "warning"),
+                        Level::Error => (Color::Red, "✖", "error"),
+                        Level::Debug => (Color::Blue, "●", "debug"),
+                        Level::Trace => (Color::Magenta, "…", "trace"),
+                    };
+                    format!(
+                        "{icon} {label}{margin} ",
+                        icon = icon.color(color),
+                        label = label.underline().color(color),
+                        // max length of label is 7 (warning)
+                        margin = " ".repeat(7 - label.len())
+                    )
+                },
+                message = message
             ))
         })
         .level(LevelFilter::Debug)
