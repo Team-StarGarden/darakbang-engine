0b23f9b600d8f184f1859e354f082d680383ea93
Joosung Park <admin@slowmotion.dev>
2020-03-26 16:36:11 +0900

    feat(Rust): Remove "dbg!" in database due to security.

--- src/database/connect.rs
+++ src/database/connect.rs
@@ -6,5 +6,5 @@ use crate::config;
 
 pub fn establish_connection(config: &config::Database) -> Result<MysqlConnection, ConnectionError> {
     let database_url = &config.url;
-    MysqlConnection::establish(dbg!(database_url))
+    MysqlConnection::establish(database_url)
 }
