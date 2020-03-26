1bd5ee8b61b84fb73fd2742b7fefa2eead9ba211
kiwiyou <kiwiyou.dev@gmail.com>
2020-03-26 19:46:01 +0900

    fix(Diesel): Add missing columns to the model

--- src/database/model.rs
+++ src/database/model.rs
@@ -6,6 +6,10 @@ pub struct User {
     pub service_name: String,
     pub user_name: String,
     pub point: i32,
+    #[diesel(deserialize_as = "HiddenColumn<Option<Vec<u8>>>")]
+    pub password: (),
+    #[diesel(deserialize_as = "HiddenColumn<Option<String>>")]
+    pub salt: (),
 }
 
 #[derive(Insertable)]
@@ -22,4 +26,30 @@ pub struct Word {
     pub service_name: String,
     pub user_name: String,
     pub point: u32,
+}
+
+use diesel::backend::Backend;
+use diesel::deserialize::Queryable;
+
+/// Represents a column which should not be serialized
+pub struct HiddenColumn<T> {
+    _data: std::marker::PhantomData<T>,
+}
+
+impl<DB, ST, T> Queryable<ST, DB> for HiddenColumn<T>
+where
+    DB: Backend,
+    T: Queryable<ST, DB>,
+{
+    type Row = T::Row;
+
+    fn build(_: Self::Row) -> Self {
+        Self {
+            _data: Default::default()
+        }
+    }
+}
+
+impl<T> Into<()> for HiddenColumn<T> {
+    fn into(self) -> () {}
 }
