b07459462a55f2bc95dea904c716b803ac08fa94
kiwiyou <kiwiyou.dev@gmail.com>
2020-03-27 14:41:09 +0900

    fix(GraphQL): Fix gql::User

--- src/gql/schema.rs
+++ src/gql/schema.rs
@@ -1,18 +1,20 @@
 use crate::database::model::User as DatabaseUser;
 
 #[derive(juniper::GraphQLObject)]
-#[graphql(description = "user object")]
+#[graphql(description = "User object")]
 pub struct User {
     uid: String,
+    service_name: String,
     user_name: String,
     point: i32,
 }
 
 impl User {
-    pub fn from_database(user: &DatabaseUser) -> Self {
+    pub fn from_database(user: DatabaseUser) -> Self {
         Self {
-            uid: user.uid.clone(),
-            user_name: user.user_name.clone(),
+            uid: user.uid,
+            service_name: user.service_name,
+            user_name: user.user_name,
             point: user.point,
         }
     }
