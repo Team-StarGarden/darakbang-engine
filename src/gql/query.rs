6b07ca054489906b1e8e825a42436c406018de6d
kiwiyou <kiwiyou.dev@gmail.com>
2020-03-27 14:49:27 +0900

    fix(GraphQL): Fix query all_users

--- src/gql/query.rs
+++ src/gql/query.rs
@@ -1,8 +1,8 @@
-use std::ptr::null;
-
-use crate::database::user::get_all_users;
 use crate::gql::Context;
 use crate::gql::schema::User;
+use crate::database::user::get_all_users;
+
+use log::error;
 
 pub struct Query;
 
@@ -14,20 +14,14 @@ impl Query {
         "1.0"
     }
 
-    fn all_users(context: &Context) -> User {
-        let conn = context.database_pool.get().unwrap();
-        let users = get_all_users(&conn);
-        match users {
-            Ok(result) => {
-                User::from_database(&result)
-            }
-            Err(error) => {
-                User {
-                    uid: "".parse().unwrap(),
-                    user_name: "".parse().unwrap(),
-                    point: 0,
-                }
-            }
-        }
+    fn all_users(context: &Context) -> Option<Vec<User>> {
+        let conn = context.database_pool
+            .get()
+            .map_err(|e| error!("Database connection failed: {}", e))
+            .ok()?;
+        get_all_users(&conn)
+            .map(|vec| vec.into_iter().map(User::from_database).collect())
+            .map_err(|e| error!("Failed to get all users: {}", e))
+            .ok()
     }
 }
