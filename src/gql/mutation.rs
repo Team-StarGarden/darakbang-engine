a2faf77ab6098fb18a651413b6535b6b8f9bc676
kiwiyou <kiwiyou.dev@gmail.com>
2020-03-27 15:07:41 +0900

    add(GraphQL): Add local account queries

--- src/gql/mutation.rs
+++ src/gql/mutation.rs
@@ -1,20 +1,33 @@
-use juniper::FieldResult;
+use log::error;
 
 use crate::gql::Context;
+use crate::gql::schema::User;
 
 pub struct Mutation;
 
 #[derive(juniper::GraphQLInputObject)]
-#[graphql(description = "Example object")]
-struct CreateUser {
-    name: String,
+#[graphql(description = "Create a local user")]
+struct CreateLocalUser {
+    user_name: String,
+    password: String,
 }
 
 #[juniper::object(
 Context = Context,
 )]
 impl Mutation {
-    fn create_user(context: &Context, new_human: CreateUser) -> FieldResult<String> {
-        Ok("Example Response".into())
+    fn create_local_user(context: &Context, new_user: CreateLocalUser) -> Option<User> {
+        use crate::database::user::create_local_user;
+        use zeroize::Zeroizing;
+        
+        let conn = context.database_pool
+            .get()
+            .map_err(|e| error!("Database connection failed: {}", e))
+            .ok()?;
+        let password = Zeroizing::new(new_user.password);
+        create_local_user(&conn, new_user.user_name, &password)
+            .map(User::from_database)
+            .map_err(|e| error!("Database connection failed: {}", e))
+            .ok()
     }
 }
--- src/gql/query.rs
+++ src/gql/query.rs
@@ -1,6 +1,5 @@
 use crate::gql::Context;
 use crate::gql::schema::User;
-use crate::database::user::get_all_users;
 
 use log::error;
 
@@ -15,6 +14,8 @@ impl Query {
     }
 
     fn all_users(context: &Context) -> Option<Vec<User>> {
+        use crate::database::user::get_all_users;
+
         let conn = context.database_pool
             .get()
             .map_err(|e| error!("Database connection failed: {}", e))
@@ -23,5 +24,21 @@ impl Query {
             .map(|vec| vec.into_iter().map(User::from_database).collect())
             .map_err(|e| error!("Failed to get all users: {}", e))
             .ok()
+    }
+
+    fn local_user(context: &Context, user_name: String, password: String) -> Option<User> {
+        use crate::database::user::find_local_user;
+        use zeroize::Zeroizing;
+
+        let conn = context.database_pool
+            .get()
+            .map_err(|e| error!("Database connection failed: {}", e))
+            .ok()?;
+        let password = Zeroizing::new(password);
+        find_local_user(&conn, &user_name, &password)
+            .map_err(|e| error!("Failed to find local user: {}", e))
+            .ok()
+            .flatten()
+            .map(User::from_database)
     }
 }
