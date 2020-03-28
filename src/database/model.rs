efac645c9b62c64efa05eecc9d4fd4103f47988d
kiwiyou <kiwiyou.dev@gmail.com>
2020-03-28 23:52:29 +0900

    add(Database): Add more word data field

--- src/database/model.rs
+++ src/database/model.rs
@@ -1,4 +1,4 @@
-use crate::database::schema::user;
+use crate::database::schema::*;
 
 #[derive(Queryable)]
 pub struct User {
@@ -16,10 +16,37 @@ pub struct NewUser {
     pub user_name: String,
 }
 
-#[derive(Queryable)]
+#[derive(Identifiable, Queryable)]
+#[table_name = "word"]
 pub struct Word {
     pub id: i32,
-    pub service_name: String,
-    pub user_name: String,
-    pub point: u32,
+    pub pyo_je_eo: String,
+    pub group_code: i32,
+    pub group_order: i32,
+    pub word_unit: String,
+    pub word_type: String,
+    pub category: Option<String>,
+    pub definition: String,
+    pub position: Option<String>,
+    pub sense_type: String,
+    pub space: String,
+}
+
+#[derive(Associations, Queryable)]
+#[belongs_to(parent = "Word")]
+#[table_name = "region_word"]
+pub struct RegionWord {
+    pub id: i32,
+    pub word_id: i32,
+    pub region: String,
+}
+
+#[derive(Associations, Queryable)]
+#[belongs_to(parent = "Word")]
+#[table_name = "original_language_word"]
+pub struct OriginalLanguageWord {
+    pub id: i32,
+    pub word_id: i32,
+    pub language: String,
+    pub original: String,
 }
